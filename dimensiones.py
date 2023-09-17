94882436022eeff708a2d01b4da402408039158c
Luis Alberto Vargas Mendez <luis.alberto.v.m@hotmail.com>
2023-09-16 21:09:06 -0500

    add dimensiones

---
+++ dimensiones.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+scalar = np.array(42)
+print(scalar)
+print(f'El número de dimensiones de un escalar es: {scalar.ndim}')
+
+vector = np.array([1, 2, 3])
+print(vector)
+print(f'El número de dimensiones de un vector es: {vector}')
