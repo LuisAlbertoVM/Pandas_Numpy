c626047fdda9f87aa156d2570a1791482bafc705
Luis Alberto Vargas Mendez <luis.alberto.v.m@hotmail.com>
2023-09-16 21:33:11 -0500

    add dimensions expand

--- dimensiones.py
+++ dimensiones.py
@@ -11,3 +11,15 @@ print(f'El número de dimensiones de un vector es: {vector.ndim}')
 matriz = np.array([[1, 2, 3], [4, 5, 6]])
 print(matriz)
 print(f'El número de dimensiones de una matriz es: {matriz.ndim}')
+
+tensor = np.array([[[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]],[[13, 13, 15], [16, 17, 18], [19, 20, 21], [22, 23, 24]]])
+print(tensor)
+print(f'El número de dimensiones de un tensor es: {tensor.ndim}')
+
+# Agregar o eliminar dimensiones
+vector = np.array([1, 2, 3], ndmin=10)
+print(vector)
+print(f'El número de dimensiones de un vector es: {vector.ndim}')
+
+expand = np.expand_dims(np.array([1, 2, 3]), axis = 0)
+print(expand)
