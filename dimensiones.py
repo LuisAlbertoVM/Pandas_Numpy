0d768c51a0fa0737a8707779d249260425a6046a
Luis Alberto Vargas Mendez <luis.alberto.v.m@hotmail.com>
2023-09-16 21:12:52 -0500

    add dimensiones and matrices

--- dimensiones.py
+++ dimensiones.py
@@ -6,4 +6,8 @@ print(f'El número de dimensiones de un escalar es: {scalar.ndim}')
 
 vector = np.array([1, 2, 3])
 print(vector)
-print(f'El número de dimensiones de un vector es: {vector}')
+print(f'El número de dimensiones de un vector es: {vector.ndim}')
+
+matriz = np.array([[1, 2, 3], [4, 5, 6]])
+print(matriz)
+print(f'El número de dimensiones de una matriz es: {matriz.ndim}')
