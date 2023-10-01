b6adfd76b898692226e28262a164c0fb9da439dd
LuisAlbertoVM <luis.alberto.v.m@hotmail.com>
2023-09-30 23:29:13 -0500

    Diferent ways to create a Dataframe

--- Series_Dataframes.py
+++ Series_Dataframes.py
@@ -3,3 +3,15 @@ import pandas as pd
 psg_players = pd.Series(['Navas', 'Mbappe', 'Neymar', 'Messi'], 
           index=[1,7,10, 39])
 print(psg_players)
+
+print()
+psg_players = pd.Series(['Navas', 'Mbappe', 'Neymar', 'Messi'])
+print(psg_players)
+
+print()
+dict = {1: 'Navas', 7: 'Mbappe', 10: 'neymar', 30: 'Messi'}
+print(pd.Series(dict))
+
+print()
+print(psg_players[0:3])
+
