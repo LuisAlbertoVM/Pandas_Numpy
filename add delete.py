5bcbc0c97cf96aded36999d9e62b5bc28eb39bf3
LuisAlbertoVM <luis.alberto.v.m@hotmail.com>
2023-10-01 10:29:37 -0500

    Add Columns

--- add delete.py
+++ add delete.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 df_books = pd.read_csv("bestsellers-with-categories.csv", sep=',', header=0)
 
@@ -19,5 +20,16 @@ print(df_books)
 print()
 print('Drop rows')
 df_books = pd.read_csv("bestsellers-with-categories.csv", sep=',', header=0)
-df_books = df_books.drop(range(0,10), axis=0)
+#df_books = df_books.drop(range(0,10), axis=0)
 print(df_books.head(2))
+
+print()
+print('Add Columns')
+df_books.head(2)
+df_books['New Column'] = np.nan
+print(df_books.shape[0])
+
+print()
+data = np.arange(0, df_books.shape[0])
+df_books['Range'] = data
+print(df_books)
