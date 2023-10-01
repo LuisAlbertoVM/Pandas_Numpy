36d595f60a616511472fde019926b05d72f086f5
LuisAlbertoVM <luis.alberto.v.m@hotmail.com>
2023-09-30 23:47:27 -0500

    create a dataframe

--- Series_Dataframes.py
+++ Series_Dataframes.py
@@ -19,5 +19,7 @@ print()
 dict = {'Jugador': ['Navas', 'Mbappe', 'Neymar', 'Messo'],
  'Altura': [183, 170, 170, 165.0],
  'Goles': [2, 200, 200, 200]}
-df_Players = pd.DataFrame(dict, index=[1, 7, 10, 30])
-print(df_Players)
+df_players = pd.DataFrame(dict, index=[1, 7, 10, 30])
+print(df_players)
+print(df_players.columns)
+print(df_players.index)
