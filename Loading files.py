794b9d01f9f1c0b26bd960a444734c218e58011e
LuisAlbertoVM <luis.alberto.v.m@hotmail.com>
2023-10-01 00:03:59 -0500

    rename columns

--- Loading files.py
+++ Loading files.py
@@ -1,2 +1,7 @@
 import pandas as pd
 
+df_books = pd.read_csv("bestsellers-with-categories.csv", sep=',', header=0, names=['Namesss', 'Authorrrr', 'User Rating', 'Reviews', 'Price', 'Year', 'Genre'])
+print(df_books)
+
+print()
+print(df_books.columns)
