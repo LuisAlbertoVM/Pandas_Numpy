12a255d23ff9d1a52d2891a63a65d31f581748f0
LuisAlbertoVM <luis.alberto.v.m@hotmail.com>
2023-10-01 10:49:01 -0500

    Know the nule values

--- Nule values.py
+++ Nule values.py
@@ -1,2 +1,11 @@
 import pandas as pd
 import numpy as np
+
+dict = {'Col1': [1,2,3,np.nan],
+ 'Col2': [4,np.nan,6,7],
+ 'Col3': ['a','b','c',None]}
+df = pd.DataFrame(dict)
+print(df)
+print()
+print(df.isnull()*1)
+df.fillna('Missing')
