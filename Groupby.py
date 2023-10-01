1406e494170d5cf5cf1157a9f154c5b2d1c888be
LuisAlbertoVM <luis.alberto.v.m@hotmail.com>
2023-10-01 12:29:02 -0500

    How to use groupby

--- Groupby.py
+++ Groupby.py
@@ -4,3 +4,31 @@ df_books = pd.read_csv("bestsellers-with-categories.csv", sep=',', header=0)
 print(df_books.head(2))
 
 print()
+print(df_books.groupby('Author').count())
+
+#print()
+#print(df_books.groupby('Author').mean())
+
+print()
+print(df_books.groupby('Author').min())
+
+print()
+print(df_books.groupby('Author').max())
+
+print()
+print(df_books.groupby('Author').sum())
+
+print()
+print(df_books.groupby('Author').sum().loc['William Davis'])
+
+print()
+print(df_books.groupby('Author').sum().reset_index())
+
+print()
+print(df_books.groupby('Author').agg(['min','max']))
+
+print()
+print(df_books.groupby('Author').agg({'Reviews':['max','max'], 'User Rating':'sum'}))
+
+print()
+print(df_books.groupby(['Author', 'Year']).count())
