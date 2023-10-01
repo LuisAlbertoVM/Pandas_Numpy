ddb1dc910164663c12c13d366c6649dd513fb596
LuisAlbertoVM <luis.alberto.v.m@hotmail.com>
2023-10-01 12:08:11 -0500

    functions info, desctibe, tail, memory_usage, value_counts, drop_duplicates, sort_values

--- Functions.py
+++ Functions.py
@@ -5,3 +5,26 @@ print(df_books.head(2))
 
 print()
 print(df_books.info())
+
+print()
+print(df_books.describe())
+
+print()
+print(df_books.tail())
+
+print()
+print(df_books.memory_usage(deep=True))
+
+print()
+print(df_books['Author'].value_counts())
+
+print()
+print(df_books.head(1))
+print()
+df_books = pd.concat([df_books,df_books.head(1)])
+print(df_books)
+
+print()
+print(df_books.drop_duplicates(keep='last'))
+
+print(df_books.sort_values('Year',ascending=False))
