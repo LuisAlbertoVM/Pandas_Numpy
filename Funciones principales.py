953fc44225263bd21be244c35b426de18a844409
LuisAlbertoVM <luis.alberto.v.m@hotmail.com>
2023-09-30 22:28:51 -0500

    add some calculous

--- Funciones principales.py
+++ Funciones principales.py
@@ -33,5 +33,26 @@ print(arr.sort())
 print()
 print(np.median(arr))
 
-
+print()
 np.median(matriz, 0)
+
+print()
+print(np.std(arr) ** 2)
+
+print()
+print(np.var(arr))
+
+print()
+np.mean(arr)
+
+print()
+a = np.array([[1,2], [3,4]])
+b = np.array([5,6])
+b = np.expand_dims(b,axis=0)
+
+print("Number of dimensions of a",a.ndim)
+print("Number of dimensions of b", b.ndim)
+print(np.concatenate((a,b),axis=0))
+
+print()
+print(np.concatenate((a,b.T),axis=1))
