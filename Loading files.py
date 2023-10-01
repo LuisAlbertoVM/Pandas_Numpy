338a6f43b7d63ceece61e8083a9c30534d0d3487
LuisAlbertoVM <luis.alberto.v.m@hotmail.com>
2023-10-01 00:17:37 -0500

    loc

--- Loading files.py
+++ Loading files.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df_books = pd.read_csv("bestsellers-with-categories.csv", sep=',', header=0, names=['Namesss', 'Authorrrr', 'User Rating', 'Reviews', 'Price', 'Year', 'Genre'])
+df_books = pd.read_csv("bestsellers-with-categories.csv", sep=',', header=0)
 print(df_books)
 
 print()
@@ -8,3 +8,11 @@ print(df_books.columns)
 
 print()
 print(pd.read_json("HPCharactersDataRaw.json", typ = 'Series'))
+
+print()
+print(df_books[0:4])
+print(df_books[['Name','Author','Year']])
+
+print()
+print("Loc")
+print(df_books.loc[:])
