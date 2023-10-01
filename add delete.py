a0b5be241aa0d2a1af873e631eda3a6fb0df6d4f
LuisAlbertoVM <luis.alberto.v.m@hotmail.com>
2023-10-01 10:24:26 -0500

    Drop Rows

--- add delete.py
+++ add delete.py
@@ -15,3 +15,9 @@ print(df_books.head(2))
 print()
 del df_books['Price']
 print(df_books)
+
+print()
+print('Drop rows')
+df_books = pd.read_csv("bestsellers-with-categories.csv", sep=',', header=0)
+df_books = df_books.drop(range(0,10), axis=0)
+print(df_books.head(2))
