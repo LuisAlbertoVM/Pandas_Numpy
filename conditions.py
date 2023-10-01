239ac27ad9010be430a25d67bdef5a9fd356daf4
LuisAlbertoVM <luis.alberto.v.m@hotmail.com>
2023-09-30 22:47:57 -0500

    Explaine the usage of conditions

--- conditions.py
+++ conditions.py
@@ -1,3 +1,9 @@
 import numpy as np
 arr = np.linspace(1, 10, 10, dtype='int8')
 print(arr)
+
+conditions_index = arr > 5
+print(conditions_index)
+
+print()
+print(arr[conditions_index & (arr < 9)])
