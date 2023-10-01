b6557b5bf7009a5946da43d8347d7b06e2ccc992
LuisAlbertoVM <luis.alberto.v.m@hotmail.com>
2023-10-01 10:20:40 -0500

    Drop Columns

---
+++ add delete.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+df_books = pd.read_csv("bestsellers-with-categories.csv", sep=',', header=0)
+
+print(df_books.head(2))
+
+print()
+print("Drop Columns")
+df_books.drop('Genre', axis=1, inplace = True)
+print(df_books.head(2))
+df_books = df_books.drop('Year',axis=1)
+print()
+print(df_books.head(2))
+
+print()
+del df_books['Price']
+print(df_books)
