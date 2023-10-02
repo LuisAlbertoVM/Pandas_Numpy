eacd0f1c5578d77433774dd5582d62404a4fa066
LuisAlbertoVM <luis.alberto.v.m@hotmail.com>
2023-10-01 19:18:10 -0500

    Use of pivot_table

--- Pivot and Melt.py
+++ Pivot and Melt.py
@@ -1,4 +1,10 @@
 import pandas as pd
 
 df_books = pd.read_csv("bestsellers-with-categories.csv", sep=',', header=0)
-print(df_books)
+print(df_books.head())
+
+print()
+print(df_books.pivot_table(index='Author',columns='Genre',values='User Rating'))
+
+print()
+print(df_books.pivot_table(index='Genre', columns='Year',values='User Rating',aggfunc='sum'))
